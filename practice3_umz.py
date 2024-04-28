10cca982a300c471a0735d6e2779577dab3d2832
mhmdrzamd <mohammadreza705682@gmail.com>
2024-04-28 21:35:42 +0330

    Update practice3_umz.py

--- practice3_umz.py
+++ practice3_umz.py
@@ -1,28 +1,30 @@
 #mohammadreza mohammadi
 dic = {'a' :[]  , 'b' : []  ,  'c' :[] ,   'd':[] }
 
-counter=0
+lie_counter=0
+
 for key in dic.keys():
 	
 	for j in range(3):
 		x = input(f' person {key} .... sentence number {j+1}.... is truth or lie:  ')
 		if x == 'truth' :
 			dic[key].append(True)
+		
 		elif x=='lie':
 			dic[key].append(False)
-			counter +=1
+			lie_counter +=1
 		else:
 			print('incorrect input.... and it concedered as a lie ')
 			dic[key].append(False)
-			counter +=1
+			lie_counter +=1
 	
-	if counter >6 :
-		print('the amount of lie is too much.... so you are wrong')
-		break
+if lie_counter != 6 :
+	print('the amount of lie is not 6 .... so you are wrong')
+	exit
  
 
 for key,value in dic.items():
 	if any(value):
 		print(f"{key} is not robber")
 	else: 
-	    print(f"{key} is the robber")
+	    print(f"{key} is the Robber")
